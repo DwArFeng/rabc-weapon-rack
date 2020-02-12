ae84f59eb920d680c9f4c8256b39c80f4f728cca
DwArFeng <915724865@qq.com>
2020-02-12 22:23:25 +0800

    ExceptionCode引入偏移量
    Signed-off-by: DwArFeng <915724865@qq.com>

--- rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/util/ServiceExceptionCodes.java
+++ rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/util/ServiceExceptionCodes.java
@@ -2,6 +2,8 @@ package com.dwarfeng.rabcwr.sdk.util;
 
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 
+import static com.dwarfeng.rabcwr.sdk.util.Constants.EXCEPTION_CODE_OFFSET;
+
 /**
  * 服务异常代码。
  *
@@ -11,26 +13,13 @@ import com.dwarfeng.subgrade.stack.exception.ServiceException;
 public final class ServiceExceptionCodes {
 
     /**
-     * 用户已经存在。
-     */
-    public static final ServiceException.Code USER_ALREADY_EXISTED = new ServiceException.Code(500, "user already existed");
-    /**
      * 用户不存在。
      */
-    public static final ServiceException.Code USER_NOT_EXISTS = new ServiceException.Code(510, "user not exists");
-    /**
-     * 密码错误。
-     */
-    public static final ServiceException.Code WRONG_PASSWORD = new ServiceException.Code(520, "wrong password");
+    public static final ServiceException.Code USER_NOT_EXISTS = new ServiceException.Code(EXCEPTION_CODE_OFFSET, "user not exists");
     /**
      * 权限表达式格式错误。
      */
-    public static final ServiceException.Code PEXP_FORMAT_ERROR = new ServiceException.Code(530, "pexp format error");
-    /**
-     * 权限表达式格式错误。
-     */
-    public static final ServiceException.Code ALREADY_EXPIRED = new ServiceException.Code(540, "already expired");
-
+    public static final ServiceException.Code PEXP_FORMAT_ERROR = new ServiceException.Code(EXCEPTION_CODE_OFFSET + 10, "pexp format error");
 
     private ServiceExceptionCodes() {
         throw new IllegalStateException("禁止实例化");
