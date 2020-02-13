52d608704ed7c594a8dacd87a4fb218ae688a43c
DwArFeng <915724865@qq.com>
2020-02-13 16:47:06 +0800

    ExceptionCode偏移量可配置化
    Signed-off-by: DwArFeng <915724865@qq.com>

--- rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/util/ServiceExceptionCodes.java
+++ rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/util/ServiceExceptionCodes.java
@@ -2,8 +2,6 @@ package com.dwarfeng.rabcwr.sdk.util;
 
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 
-import static com.dwarfeng.rabcwr.sdk.util.Constants.EXCEPTION_CODE_OFFSET;
-
 /**
  * 服务异常代码。
  *
@@ -11,6 +9,8 @@ import static com.dwarfeng.rabcwr.sdk.util.Constants.EXCEPTION_CODE_OFFSET;
  * @since 0.0.1-alpha
  */
 public final class ServiceExceptionCodes {
+
+    private static int EXCEPTION_CODE_OFFSET = 500;
 
     /**
      * 用户不存在。
@@ -20,6 +20,25 @@ public final class ServiceExceptionCodes {
      * 权限表达式格式错误。
      */
     public static final ServiceException.Code PEXP_FORMAT_ERROR = new ServiceException.Code(EXCEPTION_CODE_OFFSET + 10, "pexp format error");
+
+
+    /**
+     * 获取异常代号的偏移量。
+     *
+     * @return 异常代号的偏移量。
+     */
+    public static int getExceptionCodeOffset() {
+        return EXCEPTION_CODE_OFFSET;
+    }
+
+    /**
+     * 设置异常代号的偏移量。
+     *
+     * @param exceptionCodeOffset 指定的异常代号的偏移量。
+     */
+    public static void setExceptionCodeOffset(int exceptionCodeOffset) {
+        EXCEPTION_CODE_OFFSET = exceptionCodeOffset;
+    }
 
     private ServiceExceptionCodes() {
         throw new IllegalStateException("禁止实例化");
