db59e10cd2d95a4b5a028e280c485d747a9543f2
DwArFeng <915724865@qq.com>
2020-03-01 21:02:12 +0800

    实体对象的WebInput实现
    Signed-off-by: DwArFeng <915724865@qq.com>

---
+++ rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/bean/entity/WebInputPermission.java
@@ -0,0 +1,60 @@
+package com.dwarfeng.rabcwr.sdk.bean.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.dwarfeng.rabcwr.stack.bean.entity.Permission;
+import com.dwarfeng.subgrade.sdk.bean.key.WebInputStringIdKey;
+import com.dwarfeng.subgrade.stack.bean.Bean;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+public class WebInputPermission implements Bean {
+
+    private static final long serialVersionUID = 7403275233867303245L;
+
+    public static Permission toStackBean(WebInputPermission webInputPermission) {
+        if (Objects.isNull(webInputPermission)) {
+            return null;
+        }
+        return new Permission(
+                WebInputStringIdKey.toStackBean(webInputPermission.getKey()),
+                webInputPermission.getRemark()
+        );
+    }
+
+    @JSONField(name = "key")
+    @Valid
+    @NotNull
+    private WebInputStringIdKey key;
+
+    @JSONField(name = "remark")
+    private String remark;
+
+    public WebInputPermission() {
+    }
+
+    public WebInputStringIdKey getKey() {
+        return key;
+    }
+
+    public void setKey(WebInputStringIdKey key) {
+        this.key = key;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "WebInputPermission{" +
+                "key=" + key +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+}
---
+++ rabc-weapon-rack-sdk/src/main/java/com/dwarfeng/rabcwr/sdk/bean/entity/WebInputPexp.java
@@ -0,0 +1,92 @@
+package com.dwarfeng.rabcwr.sdk.bean.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.dwarfeng.rabcwr.stack.bean.entity.Pexp;
+import com.dwarfeng.subgrade.sdk.bean.key.WebInputLongIdKey;
+import com.dwarfeng.subgrade.sdk.bean.key.WebInputStringIdKey;
+import com.dwarfeng.subgrade.stack.bean.Bean;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.Default;
+import java.util.Objects;
+
+public class WebInputPexp implements Bean {
+
+    private static final long serialVersionUID = 955803288564850694L;
+
+    public static Pexp toStackBean(WebInputPexp webInputPexp) {
+        if (Objects.isNull(webInputPexp)) {
+            return null;
+        }
+        return new Pexp(
+                WebInputLongIdKey.toStackBean(webInputPexp.getKey()),
+                WebInputStringIdKey.toStackBean(webInputPexp.getRoleKey()),
+                webInputPexp.getContent(),
+                webInputPexp.getRemark()
+        );
+    }
+
+    @JSONField(name = "key")
+    @Valid
+    @NotNull(groups = Default.class)
+    private WebInputLongIdKey key;
+
+    @JSONField(name = "role_key")
+    @Valid
+    private WebInputStringIdKey roleKey;
+
+    @JSONField(name = "content")
+    @NotNull
+    @NotEmpty
+    private String content;
+
+    @JSONField(name = "remark")
+    private String remark;
+
+    public WebInputPexp() {
+    }
+
+    public WebInputLongIdKey getKey() {
+        return key;
+    }
+
+    public void setKey(WebInputLongIdKey key) {
+        this.key = key;
+    }
+
+    public WebInputStringIdKey getRoleKey() {
+        return roleKey;
+    }
+
+    public void setRoleKey(WebInputStringIdKey roleKey) {
+        this.roleKey = roleKey;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public String toString() {
+        return "WebInputPexp{" +
+                "key=" + key +
+                ", roleKey=" + roleKey +
+                ", content='" + content + '\'' +
+                ", remark='" + remark + '\'' +
+                '}';
+    }
+}
